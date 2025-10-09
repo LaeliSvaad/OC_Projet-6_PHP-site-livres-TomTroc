900912749a60229cd86a7af108f5afdeda03d3f2
LaeliSvaad <ls@laelisvaad.com>
2025-10-09 14:03:13 +0200

    book-details template

--- views/templates/book-details.php
+++ views/templates/book-details.php
@@ -1,22 +1,39 @@
 <?php
-echo "<h2>" . $book->getTitle() . "</h2>";
-echo "<img class='book-img' src='" . $book->getBookPicture() . "' alt='" . $book->getTitle() . "'>";
-echo "<p>Auteur: " . $book->getAuthor()->getFirstname() . " " . $book->getAuthor()->getLastname() . " " .  $book->getAuthor()->getPseudo() . "</p>";
-echo "<p>Description: " . $book->getDescription() . "</p>";
-if(isset($_SESSION["user"]) && $_SESSION["user"] == $book->getUser()->getUserId()){
-    echo "<p>Vendu par vous</p>";
-}
-else{
-    echo "<p>Vendu par : <a href='index.php?action=user-public-account&id=". $book->getUser()->getUserId() ."' >" . $book->getUser()->getNickname() . "</a> - ";
-}
-if(!isset($_SESSION["user"])){
-    echo "Créez un compte ou connectez-vous pour lui envoyer un message.</p>";
-}
-else if(isset($_SESSION["user"]) && $_SESSION["user"] === $book->getUser()->getUserId()){
-    echo"<a href='index.php?action=edit-book&bookId=" . $book->getId() . "'>Editer les détails de ce livre</a></p>";
-}
-else{
-        echo "<a href='index.php?action=conversation&user1Id=" . $book->getUser()->getUserId() . "&user2Id=" . $_SESSION["user"] . "'>Lui envoyer un message</a></p>";
-}
-
-
+$connectedUserId = $_SESSION["user"] ?? null;
+$author = $book->getAuthor();
+$user = $book->getUser();
+$isOwner = $connectedUserId === $user->getUserId();
+$bookPicture = $book->getBookPicture();
+$bookTitle = $book->getTitle();
+$bookDescription = $book->getDescription();
+$userPicture = $user->getPicture();
+$userNickname = $user->getNickname();
+$userId = $user->getUserId();
+?>
+<section>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-sm-6'>
+                <img class='book-img' src='<?= $bookPicture ?>' alt='<?= $bookTitle ?>'>
+            </div>
+            <div class="col-sm-6">
+                <h2 class="playfair-display-title-font"><?= $bookTitle ?></h2>
+                <span>par <strong><?= $author->getFirstname() ?> <?= $author->getLastname() ?></strong></span>
+                <span>Description</span>
+                <p><?= $bookDescription ?></p>
+                <span>Propriétaire</span>
+                <div>
+                    <a href='<?= $isOwner ? "index.php?action=user-private-account" : "index.php?action=user-public-account&id=$userId" ?>'>
+                        <img src='<?= $userPicture ?>' alt='<?= $userNickname ?>'>
+                        <span><?= $isOwner ? "Vous" : $userNickname ?></span>
+                    </a>
+                    <?= !$connectedUserId
+                        ? "<p>Créez un compte ou connectez-vous pour lui envoyer un message.</p>"
+                        : (!$isOwner
+                            ? "<a class='button-link' href='index.php?action=conversation&user1Id=$userId&user2Id=$connectedUserId'><button class='btn green-button'>Envoyer un message</button></a>"
+                            : "") ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
